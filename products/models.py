ced1456dfd8b3d200da2cf74c18fbf166e91e1c1
eskokado <eskokado@gmail.com>
2023-04-24 07:10:31 -0300

    feat: create model Product

--- products/models.py
+++ products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, null=False, unique=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+
+    class Meta:
+        db_table = 'products'
