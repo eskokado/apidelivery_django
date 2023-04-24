721400182ed94a996c2e94304a0d601f2bcdbc64
eskokado <eskokado@gmail.com>
2023-04-24 08:38:35 -0300

    feat: create model OrderItem

--- order_items/models.py
+++ order_items/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from orders.models import Order
+from products.models import Product
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    discount = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+
+    class Meta:
+        db_table = 'order_items'
+        constraints = [
+            models.UniqueConstraint(fields=['order', 'product'], name='unique_order_product')
+        ]
