5951bcf0b37d7c408fe62a048d5d4b0a1360c734
eskokado <eskokado@gmail.com>
2023-04-24 16:40:40 -0300

    feat: create view DeliveryView

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import CreateDeliveryRequest, CreateDeliveryResponse
+from .delivery_service import DeliveryService
+
+
+class DeliveryView(APIView):
+    def post(self, request):
+        serializer = CreateDeliveryRequest(data=request.data)
+        if serializer.is_valid():
+            delivery_response_data = DeliveryService.create_delivery(serializer.validated_data)
+            response_serializer = CreateDeliveryResponse(data=delivery_response_data)
+            if response_serializer.is_valid():
+                return Response(response_serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response(response_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
