440b0b3230da105838caefaecb6a400a0de782a2
eskokado <eskokado@gmail.com>
2023-04-24 16:37:44 -0300

    fix: adjusting variables DJANGO_APPS, THIRD_PARTY_APPS, MY_APPS and INSTALLED_APPS

--- _core/settings.py
+++ _core/settings.py
@@ -30,13 +30,20 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+]
+
+THIRD_PARTY_APPS = [
+    "rest_framework",
+]
+
+MY_APPS = [
     "addresses",
     "clients",
     "suppliers",
@@ -44,6 +51,8 @@ INSTALLED_APPS = [
     "orders",
     "order_items",
 ]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + MY_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
