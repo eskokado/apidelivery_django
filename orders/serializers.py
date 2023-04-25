f27a6eefedb1014c0f0d4b2913b68851d81eeabc
eskokado <eskokado@gmail.com>
2023-04-25 07:14:04 -0300

    fix: fix serializer Order

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,6 +1,10 @@
 from rest_framework import serializers
 
+from addresses.serializers import AddressSerializer
+from order_items.models import OrderItem
 from order_items.serializers import OrderItemSerializer
+from products.serializers import ProductSerializer
+from suppliers.serializers import SupplierSerializer
 from .models import Order
 
 
@@ -17,15 +21,42 @@ class ResponseError(serializers.Serializer):
     field = serializers.CharField()
 
 
-class OrderSerializer(serializers.ModelSerializer):
+class OrderResponseSerializer(serializers.ModelSerializer):
+    address_of_delivery = AddressSerializer(read_only=True)
+    supplier = SupplierSerializer(read_only=True)
+
     class Meta:
         model = Order
         fields = '__all__'
 
 
+class OrderItemResponseSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    order_items = OrderItemSerializer(many=True, read_only=True)
+    supplier = SupplierSerializer(read_only=True)
+    address_of_delivery = AddressSerializer(read_only=True)
+
+    class Meta:
+        model = Order
+        fields = ('id', 'order_date', 'state_delivery', 'address_of_delivery', 'supplier', 'order_items')
+
+
 class CreateDeliveryResponse(serializers.Serializer):
+    order = OrderResponseSerializer()
+    order_items = OrderItemResponseSerializer(many=True)
+    errors = serializers.ListField(child=serializers.DictField(), required=False)
+    status_code = serializers.IntegerField()
+
+
+class DeliveryResponseSerializer(serializers.Serializer):
     order = OrderSerializer()
     order_items = OrderItemSerializer(many=True)
     errors = serializers.ListField(child=serializers.DictField(), required=False)
     status_code = serializers.IntegerField()
-
