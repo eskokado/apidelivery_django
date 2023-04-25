f42e269b250c4495ee9d5ef4616f9b51b80eaf9a
eskokado <eskokado@gmail.com>
2023-04-25 07:10:45 -0300

    fix: fix create sub create_delivery_response

--- orders/delivery_service.py
+++ orders/delivery_service.py
@@ -1,18 +1,36 @@
-import ipdb
 from django.core.exceptions import ObjectDoesNotExist
 
 from addresses.models import Address
+from addresses.serializers import AddressSerializer
 from clients.models import Client
 from order_items.models import OrderItem
-from order_items.serializers import OrderItemSerializer
 from orders.models import Order
-from orders.serializers import OrderSerializer
+from orders.serializers import OrderSerializer, OrderResponseSerializer, OrderItemResponseSerializer
 from products.models import Product
+from products.serializers import ProductSerializer
 from suppliers.models import Supplier
+from suppliers.serializers import SupplierSerializer
+
+
+def create_delivery_response(order, order_item):
+    order_data = OrderResponseSerializer(order).data
+    order_item_data = OrderItemResponseSerializer(order_item).data
+
+    order_data["address_of_delivery"] = AddressSerializer(order.address_of_delivery).data
+    order_data["supplier"] = SupplierSerializer(order.supplier).data
+    order_item_data["product"] = ProductSerializer(order_item.product).data
+
+    response = {
+        'order': order_data,
+        'order_items': [order_item_data],
+        'errors': [],
+        'status_code': 201
+    }
+
+    return response
 
 
 class DeliveryService:
-
     @staticmethod
     def create_delivery(delivery_request):
         delivery_response = {
@@ -40,12 +58,15 @@ class DeliveryService:
             )
             order_item.save()
 
-            serialized_order = OrderSerializer(order).data
-            serialized_order_item = OrderItemSerializer(order_item).data
+            serialized_order = OrderResponseSerializer(order).data
+            serialized_order['address_of_delivery'] = AddressSerializer(address).data
+            serialized_order['supplier'] = SupplierSerializer(supplier).data
+
+            serialized_order_item = OrderItemResponseSerializer(order_item).data
+            serialized_order_item['product'] = ProductSerializer(product).data
 
             delivery_response['order'] = serialized_order
             delivery_response['order_items'].append(serialized_order_item)
-            delivery_response['status_code'] = 201
         except ObjectDoesNotExist:
             delivery_response['errors'].append({'message': 'Objeto não encontrado', 'field': None})
             delivery_response['status_code'] = 400
