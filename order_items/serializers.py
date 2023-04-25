468abbfdaf2d225d6a0f6afecac525beebd322e1
eskokado <eskokado@gmail.com>
2023-04-25 07:14:11 -0300

    fix: fix serializer OrderItem

--- order_items/serializers.py
+++ order_items/serializers.py
@@ -1,14 +1,11 @@
 from rest_framework import serializers
 from order_items.models import OrderItem
+from products.serializers import ProductSerializer
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
-    order_id = serializers.IntegerField(source='order.id', read_only=True)
-    product_id = serializers.IntegerField(source='product.id', read_only=True)
+    product = ProductSerializer(read_only=True)
 
     class Meta:
         model = OrderItem
-        fields = ['id', 'order_id', 'product_id', 'discount', 'quantity', 'price']
-
-    def validate(self, data):
-        return data
+        fields = ('id', 'order', 'product', 'discount', 'quantity', 'price')
