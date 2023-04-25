111c0d32a241f9589479039cd0ae42e78a9ed5f0
eskokado <eskokado@gmail.com>
2023-04-25 07:14:33 -0300

    fix: fix view DeliveryView

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,10 @@
+import ipdb
 from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import CreateDeliveryRequest, CreateDeliveryResponse
+from .serializers import CreateDeliveryRequest, CreateDeliveryResponse, DeliveryResponseSerializer
 from .delivery_service import DeliveryService
 
 
@@ -12,10 +13,6 @@ class DeliveryView(APIView):
         serializer = CreateDeliveryRequest(data=request.data)
         if serializer.is_valid():
             delivery_response_data = DeliveryService.create_delivery(serializer.validated_data)
-            response_serializer = CreateDeliveryResponse(data=delivery_response_data)
-            if response_serializer.is_valid():
-                return Response(response_serializer.data, status=status.HTTP_201_CREATED)
-            else:
-                return Response(response_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(delivery_response_data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
